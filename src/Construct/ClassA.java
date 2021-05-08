ca050d860ab8c8eb9a1ac9566f4ae393a8abb332
QA <gkh@asf.com>
2021-05-08 08:41:23 +0530

    Third commit

--- src/Construct/ClassA.java
+++ src/Construct/ClassA.java
@@ -1,9 +1,20 @@
 package Construct;
 
 public class ClassA {
+	
+	String name;
+	int age ;
 
-	public ClassA() {
+	public ClassA(String name , int age) {
+		
+		this.age =age;
+		this.name =name;
+		System.out.println("Name = "+ name);
+		System.out.println("Age = "+ age);
 		
 	}
 
+	public void details() {
+		System.out.println(name+ age);
+	}
 }
--- src/Construct/Execute.java
+++ src/Construct/Execute.java
@@ -4,6 +4,10 @@ public class Execute {
 
 	public static void main(String[] args) {
 		
+		ClassA clA=new ClassA("Shahab", 37);
+		
+	
+		clA.details();
 
 	}
 
